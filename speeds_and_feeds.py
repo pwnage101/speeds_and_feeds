ea9d866389cba9114d0a4488940b9adbff5bf421
Troy Sankey <sankeytms@gmail.com>
2018-05-10 16:12:39 -0400

    parameterize machines, add the sharp and the bridgeport

--- speeds_and_feeds.py
+++ speeds_and_feeds.py
@@ -72,95 +72,137 @@ materials = OrderedDict([
 ])
 
 # machine properties
-machine_horsepower = 0.5 * ureg.hp  # about half of the motor's rated horsepower
-machine_efficiency = 0.75  # dimensionless fraction
-machine_speed_max = 2500 * ureg.rev / ureg.min
-machine_feed_max = 60 * ureg.inch / ureg.min
 
-with PdfPages('speeds_and_feeds.pdf') as pdf:
-    for tool in tools:
-        # X axis values
-        doc_axial = np.arange(0.0, MAX_DOC_AXIAL * tool['diameter'].to(ureg.inch).magnitude + 0.01, 0.01) * ureg.inch
+# This multiplier gives us our safety buffer.  We're trying to operate the
+# machine within the max ratings, so any calculation errors due to
+# poor approximations won't overload the motor and cause stalls or reduce
+# machine/tool lifespan.
+machine_horsepower_multiplier = 0.5
 
-        fig, ax = plt.subplots(figsize=(9, 8))
+# Represents the fact that the motor doesn't perfectly transfer power to the
+# spindle.  There are always friction losses at the belt, and inside the motor
+# and spindle bearings.
+machine_efficiency = 0.75
 
-        for material_name, material_properties in materials.items():
-            SFM = material_properties['SFM']
+machines = [
+    {
+        'name': 'Sharp LMV CNC Mill',
+        'horsepower': 3.0 * ureg.hp,
+        'feed_max': 60 * ureg.inch / ureg.min,
+        'vari_speed_max': 4000 * ureg.rev / ureg.min,
+    },
+    {
+        'name': 'Bridgeport J-Head Mill',
+        'horsepower': 1.0 * ureg.hp,
+        'feed_max': 30 * ureg.inch / ureg.min,
+        'speeds': [80, 135, 210, 325, 660, 1115, 1750, 2720] * ureg.rev / ureg.min,
+    },
+]
 
-            # For simplicity, we say carbide tools can sustain double the SFM
-            # of HSS tools.  In reality, this varies widely with different
-            # material types and tool coatings, but 2x surface speed is a good
-            # conservative estimate.  In production environments, some people
-            # are running carbide 4-5x.
-            if tool['material'] is 'Carbide':
-                SFM *= 2.0
+def closest_machine_speed(speed, machine):
+    """
+    For a given ideal spindle speed, return the nearest one which is physically
+    possible.
+    """
+    if 'speeds' in machine:
+        return min(machine['speeds'], key=lambda x: abs(x-speed))
+    else:
+        if speed > machine['vari_speed_max']:
+            return machine['vari_speed_max']
+        else:
+            return speed
 
-            speed = SFM / ( pi * tool['diameter'] / ureg.rev )
+def target_spindle_horsepwoer(machine):
+    return machine['horsepower'] * machine_horsepower_multiplier * machine_efficiency
 
-            if speed > machine_speed_max:
-                speed = machine_speed_max
+for machine in machines:
 
-            feed = 0.005 * tool['diameter'] * speed * (tool['tooth_count'] / ureg.rev)
+    machine_filename_suffix = machine['name'].replace(' ', '_')
 
-            if feed > machine_feed_max:
-                feed = machine_feed_max
+    with PdfPages('speeds_and_feeds_{}.pdf'.format(machine_filename_suffix)) as pdf:
 
-            MRR = machine_horsepower * machine_efficiency / material_properties['unit_power']
-            doc_radial = MRR / feed / doc_axial
-            stepover = 100 * (doc_radial / tool['diameter']).to(ureg.dimensionless)
+        # Add a new graph on a new page for each different tool.
+        for tool in tools:
+            # X axis values
+            doc_axial = np.arange(0.0, MAX_DOC_AXIAL * tool['diameter'].to(ureg.inch).magnitude + 0.01, 0.01) * ureg.inch
 
-            ax.plot(doc_axial, stepover, label='{}: {:.0f} RPM, {:.1f} IPM'.format(
-                material_name,
-                speed.to(ureg.rev / ureg.min).magnitude,
-                feed.to(ureg.inch / ureg.min).magnitude,
-                ))
+            fig, ax = plt.subplots(figsize=(9, 8))
 
-        ax.legend()
+            # Plot a new line for each different type of material.
+            for material_name, material_properties in materials.items():
+                SFM = material_properties['SFM']
 
-        ax.set(xlabel='DOC, Axial (inch)', ylabel='Stepover (%)',
-               title='Milling Parameters, {:.2f} hp, {}" {:d} fl. {} End Mill'.format(
-                   machine_horsepower.magnitude,
-                   Fraction(tool['diameter'].magnitude).limit_denominator(),
-                   int(tool['tooth_count']),
-                   tool['material'],
-                   ))
+                # For simplicity, we say carbide tools can sustain double the SFM
+                # of HSS tools.  In reality, this varies widely with different
+                # material types and tool coatings, but 2x surface speed is a good
+                # conservative estimate.  In production environments, some people
+                # are running carbide 4-5x.
+                if tool['material'] is 'Carbide':
+                    SFM *= 2.0
 
-        # X limits are proportional to tool diameter.
-        ax.set_xlim(0, MAX_DOC_AXIAL * tool['diameter'].to(ureg.inch).magnitude)
+                speed = SFM / ( pi * tool['diameter'] / ureg.rev )
+                speed = closest_machine_speed(speed, machine)
 
-        # Do some magic to get perfect x axis tick granularity.
-        tick_step = 0.0001
-        for _ in range(10):
-            if tick_step*10 > ax.get_xlim()[1]/8:
-                tick_step *= 5
-                break
-            if tick_step*20 > ax.get_xlim()[1]/8:
+                feed = 0.005 * tool['diameter'] * speed * (tool['tooth_count'] / ureg.rev)
+                if feed > machine['feed_max']:
+                    feed = machine['feed_max']
+
+                MRR = target_spindle_horsepwoer(machine) * machine_efficiency / material_properties['unit_power']
+                doc_radial = MRR / feed / doc_axial
+                stepover = 100 * (doc_radial / tool['diameter']).to(ureg.dimensionless)
+
+                ax.plot(doc_axial, stepover, label='{}: {:.0f} RPM, {:.1f} IPM'.format(
+                    material_name,
+                    speed.to(ureg.rev / ureg.min).magnitude,
+                    feed.to(ureg.inch / ureg.min).magnitude,
+                    ))
+
+            ax.legend()
+
+            ax.set(xlabel='DOC, Axial (inch)', ylabel='Stepover (%)',
+                   title='Milling Parameters, {:.2f} hp, {}" {:d} fl. {} End Mill'.format(
+                       target_spindle_horsepwoer(machine).magnitude,
+                       Fraction(tool['diameter'].magnitude).limit_denominator(),
+                       int(tool['tooth_count']),
+                       tool['material'],
+                       ))
+
+            # X limits are proportional to tool diameter.
+            ax.set_xlim(0, MAX_DOC_AXIAL * tool['diameter'].to(ureg.inch).magnitude)
+
+            # Do some magic to get perfect x axis tick granularity.
+            tick_step = 0.0001
+            for _ in range(10):
+                if tick_step*10 > ax.get_xlim()[1]/8:
+                    tick_step *= 5
+                    break
+                if tick_step*20 > ax.get_xlim()[1]/8:
+                    tick_step *= 10
+                    break
+                if tick_step*50 > ax.get_xlim()[1]/8:
+                    tick_step *= 20
+                    break
                 tick_step *= 10
-                break
-            if tick_step*50 > ax.get_xlim()[1]/8:
-                tick_step *= 20
-                break
-            tick_step *= 10
-        ax.set_xticks(np.arange(0, ax.get_xlim()[1], tick_step))
+            ax.set_xticks(np.arange(0, ax.get_xlim()[1], tick_step))
 
-        # Limit display up to 100% stepover since that's the entire diameter of
-        # the endmill (slot milling).
-        ax.set_ylim(0, 100)
-        ax.set_yticks(np.arange(0, 101, step=5))
+            # Limit display up to 100% stepover since that's the entire diameter of
+            # the endmill (slot milling).
+            ax.set_ylim(0, 100)
+            ax.set_yticks(np.arange(0, 101, step=5))
 
-        ax.grid()
+            ax.grid()
 
-        # Setup secondary axis on right side of graph to show physical stepover.
-        ax2 = ax.twinx()
-        ax2.set_ylim(0, tool['diameter'].magnitude)
-        yticks = np.arange(0, tool['diameter'].magnitude*1.05, step=tool['diameter'].magnitude*0.05)
-        ax2.set_yticks(yticks)
-        ax2.set_yticklabels([
-            '{:.3f} [{:.2f}]'.format(
-                tick,
-                (tick * ureg.inch).to(ureg.mm).magnitude
-            ) for tick in yticks])
-        ax2.set_ylabel('Physical Stepover (inch [mm])')
+            # Setup secondary axis on right side of graph to show physical stepover.
+            ax2 = ax.twinx()
+            ax2.set_ylim(0, tool['diameter'].magnitude)
+            yticks = np.arange(0, tool['diameter'].magnitude*1.05, step=tool['diameter'].magnitude*0.05)
+            ax2.set_yticks(yticks)
+            ax2.set_yticklabels([
+                '{:.3f} [{:.2f}]'.format(
+                    tick,
+                    (tick * ureg.inch).to(ureg.mm).magnitude
+                ) for tick in yticks])
+            ax2.set_ylabel('Physical Stepover (inch [mm])')
 
-        fig.tight_layout()  # otherwise the right y-label is slightly clipped
-        pdf.savefig(fig)
+            fig.tight_layout()  # otherwise the right y-label is slightly clipped
+            pdf.savefig(fig)
